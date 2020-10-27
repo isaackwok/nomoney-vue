97a6cecf4cc63ec3fe65c6222fffff4b831b3e06
Isaac Kwok <isaac062522@gmail.com>
2020-10-27 05:16:56 +0000

    feat: remove code only works in dev mode

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,9 @@
 <template>
-  <!-- <div v-if="web"> --> 
+  <!-- <div v-if="web"> -->
   <div v-if="true">
     <NavBar />
     <div
-      v-if="userProfile && ( !userProfile.phone || !userProfile.lineId)"
+      v-if="userProfile && (!userProfile.phone || !userProfile.lineId)"
       class="bg-red-300 text-red-700 p-2 text-sm"
     >
       請記得更新你的資料，才能夠配對得更準確，更容易應徵成功喔！
@@ -16,7 +16,8 @@
   >
     <img src="~/assets/line_account_qrcode.png" />
     <p class="text-gray-600 mt-3">
-      請加入「<span class="text-green-500">{{ process.env.prjname }}</span>」LINE好友
+      請加入「<span class="text-green-500">{{ process.env.prjname }}</span
+      >」LINE好友
     </p>
     <p class="text-green-500">LINE ID: @832gobav</p>
   </div>
@@ -43,78 +44,42 @@ export default {
       if (process.env.dev) return false;
       return this.$store.state.OS === "web";
     },
-    userProfile(){
+    userProfile() {
       return this.$store.state.userProfile;
-    }
+    },
   },
   mounted() {
-    // if SSR
-    if (process.env.dev) {
-      this.$store.commit("setOS", liff);
-      liff
-        .init({
-          liffId: LIFF_ID,
-        })
-        .then(async () => {
-          this.$store.commit("setUserIdToken", {
-            userId: "U04d1a0375336023979bce781d7da76b3",
-            displayName: "Isaac Kwok",
-            pictureUrl:
-              "https://profile.line-scdn.net/0h2jM2kFEJbUVwS0Qla_0SEkwOYygHZWsNCHggIQYeMyFeLi1DSn8rdFJCNCUIeipGG311JFVIY30P",
-            statusMessage: "我\n超帥",
-          });
-          this.$store.commit(
-            "setUserProfile",
-            await fetch("/api/crud_profile", {
-              method: "POST",
-              body: JSON.stringify({
-                action: "create",
-                userIdToken: this.$store.state.userIdToken.userId,
-                displayName: this.$store.state.userIdToken.displayName,
-                image: this.$store.state.userIdToken.pictureUrl
-                  ? this.$store.state.userIdToken.pictureUrl
-                  : "",
-              }),
-            }).then((res) => res.json())
-          );
-        })
-        .catch((err) => {
-          // do something when error is catched
-          console.error(err);
-        });
-    } else {
-      this.$store.commit("setOS", liff);
-      // initialize LIFF
-      liff
-        .init({
-          liffId: LIFF_ID,
-        })
-        .then(async () => {
-          // handle login
-          if (liff.isLoggedIn()) {
-            this.$store.commit("setUserIdToken", await liff.getProfile());
-            if (!this.$store.state.userProfile) {
-              this.$store.commit(
-                "setUserProfile",
-                await fetch("/api/crud_profile", {
-                  method: "POST",
-                  body: JSON.stringify({
-                    action: "create",
-                    userIdToken: this.$store.state.userIdToken.userId,
-                    displayName: this.$store.state.userIdToken.displayName,
-                    image: this.$store.state.userIdToken.pictureUrl
-                      ? this.$store.state.userIdToken.pictureUrl
-                      : "",
-                  }),
-                }).then((res) => res.json())
-              );
-            }
+    this.$store.commit("setOS", liff);
+    // initialize LIFF
+    liff
+      .init({
+        liffId: LIFF_ID,
+      })
+      .then(async () => {
+        // handle login
+        if (liff.isLoggedIn()) {
+          this.$store.commit("setUserIdToken", await liff.getProfile());
+          if (!this.$store.state.userProfile) {
+            this.$store.commit(
+              "setUserProfile",
+              await fetch("/api/crud_profile", {
+                method: "POST",
+                body: JSON.stringify({
+                  action: "create",
+                  userIdToken: this.$store.state.userIdToken.userId,
+                  displayName: this.$store.state.userIdToken.displayName,
+                  image: this.$store.state.userIdToken.pictureUrl
+                    ? this.$store.state.userIdToken.pictureUrl
+                    : "",
+                }),
+              }).then((res) => res.json())
+            );
           }
-        })
-        .catch((err) => {
-          // do something when error is catched
-        });
-    }
+        }
+      })
+      .catch((err) => {
+        // do something when error is catched
+      });
   },
 };
 </script>
