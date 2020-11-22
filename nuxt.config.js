00f3c1c501a1b554f457ebe8d3072e4f34d65c06
Isaac Kwok <isaac062522@gmail.com>
2020-11-22 02:27:03 +0000

    feat: set `baseUrl` in `nuxt.config.js`

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,7 +4,7 @@ export default {
     LIFF_ID: '1655089903-YawqnnaN',
     dev: process.env.NODE_ENV !== 'production',
     prjname: '森上梅友前',
-    baseUrl: process.env.BASE_URL || ''
+    baseUrl: process.env.NODE_ENV === 'production'? '': 'https://nomoney.nlplab.cc'
   },
   buildModules: ['@nuxtjs/tailwindcss'],
   css: ['~/assets/page-transition.css']
