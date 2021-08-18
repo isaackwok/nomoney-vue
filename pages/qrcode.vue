4082b8ed636a16d26b073791510e777fd917deec
Isaac Kwok <isaac062522@gmail.com>
2021-08-18 14:46:48 +0000

    fix: add margin above arcode

--- pages/qrcode.vue
+++ pages/qrcode.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex flex-col p-4 justify-center">
-    <p class="text-xl text-center text-gray-700">掃描 QR Code 加入 LINE Bot 以享用更完整的功能！</p>
+    <p class="mb-4 text-xl text-center text-gray-700">掃描 QR Code 加入 LINE Bot 以享用更完整的功能！</p>
     <img src="@/assets/nomoney-qrcode.png" />
   </div>
 </template>
